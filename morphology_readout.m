858758b80420e34f5458c053acf854769715d6a7
Tristan Chaplin <tristan.chaplin@gmail.com>
2022-03-09 08:44:43 +0000

    WIP reorienting cells

--- morphology_readout.m
+++ morphology_readout.m
@@ -23,6 +23,10 @@ elseif contains(swc_list{1,i},'basal','IgnoreCase',true)==1;
  branchtype(i)=1;
 elseif contains(swc_list{1,i},'soma','IgnoreCase',true)==1;
  branchtype(i)=2;
+elseif contains(swc_list{1,i},'surface','IgnoreCase',true)==1;
+ branchtype(i)=3;
+elseif contains(swc_list{1,i},'axon','IgnoreCase',true)==1;
+ branchtype(i)=4;
 else 
   disp('file names need to contain either apical, basal or soma');
 end
@@ -30,6 +34,8 @@ end
 apical=find(branchtype==0);
 basal=find(branchtype==1);
 soma=find(branchtype==2);
+surface=find(branchtype==3);
+axon=find(branchtype==4);
 %% Extract parameters for apical and basal using stats_tree from TREES toolbox
 %basal: all basal trees are first concatenated-> one basal tree
 com_tree=[];
@@ -54,19 +60,17 @@ else
 end
 com_tree.name='basal_combined';
 
-%stats see https://www.treestoolbox.org/downloads/TREES_manual.pdf
-%page 75
-%apical
-for i=1:length(apical)
-apical_tree=[];
-[apical_tree] = load_tree (swc_list{1,apical(i)}, 'swc');
-% Removed options '-x' here because we want sholl.
-apical_stats=stats_tree_sw(apical_tree,[],[],'-w');close all;
+
+surface_tree = [];
+if ~isempty(surface)
+    [surface_tree] = load_tree (swc_list{1,surface(1)}, 'swc');
 end
-%basal
-% Removed option '-x' here because we want sholl.
-basal_stats=stats_tree_sw(com_tree,[],[],'-w');close all;
-basal_stats.gstats.basaltrees=length(basal);
+
+axon_tree = [];
+if ~isempty(axon)
+    [axon_tree] = load_tree (swc_list{1,axon(1)}, 'swc');
+end
+
 %% Morphology soma subtraction
 [soma_tree] = load_tree (swc_list{1,soma(1)}, 'swc');
 %get a single coordinates for the soma (in this case the mean)
@@ -86,6 +90,42 @@ soma_tree.Y=soma_tree.Y-my;
 soma_tree.Z=soma_tree.Z-mz;
 morphology_traces={apical_tree;com_tree;soma_tree};
 
+if ~isempty(surface)
+    %         surface_tree.X = medfilt1(surface_tree.X, 10);
+    %         surface_tree.Y = medfilt1(surface_tree.Y, 10);
+    
+    surfPts = [surface_tree.X surface_tree.Y];
+    somaPt = [morphData.soma_stats.mx morphData.soma_stats.my];
+    [k, dist] = dsearchn(surfPts, somaPt);
+    nClosePts = 500;
+    closestPt = surfPts(k);
+    surfPtsClose = surfPts(k - nClosePts:k+nClosePts);
+    c = polyfit(surfPtsClose(:, 1), surfPtsClose(:, 2), 1);
+    m = c(1);
+    surfAngleRad = atan(m);
+    surfAngleDeg = rad2deg(surfAngleRad);
+end
+
+
+%stats see https://www.treestoolbox.org/downloads/TREES_manual.pdf
+%page 75
+%apical
+for i=1:length(apical)
+apical_tree=[];
+[apical_tree] = load_tree (swc_list{1,apical(i)}, 'swc');
+% Removed options '-x' here because we want sholl.
+apical_stats=stats_tree_sw(apical_tree,[],[],'-w');close all;
+end
+%basal
+% Removed option '-x' here because we want sholl.
+basal_stats=stats_tree_sw(com_tree,[],[],'-w');close all;
+basal_stats.gstats.basaltrees=length(basal);
+
+
+
+
+
+
 if plot_cell
     % %plot the cell itself 
     big=figure;mon_pos=get(0,'MonitorPositions');set(gcf,'color','w', 'menubar','figure', 'position',[mon_pos(1,3)-1200 2 500 500]); % [left, bottom, width, height]
@@ -94,6 +134,9 @@ end
 %save data in cd folder as morph_data (traces + stats)
 cd(swc_list{1,apical(1)}(1:end-(length(apical_tree.name)+4)));
 morph_data.traces=morphology_traces;
+morph_data.traces_surface=surface_tree;
+morph_data.traces_axon=axon_tree;
+
 morph_data.apical_stats=apical_stats;
 morph_data.basal_stats=basal_stats;
 morph_data.soma_stats.mx=mx;
